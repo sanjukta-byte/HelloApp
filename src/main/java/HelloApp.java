4c4edd2627c311526da176a7accaa2c62a02b39a
sanjukta-byte <guddubiswas1969@gmail.com>
2026-04-10 08:57:08 +0530

    Coded UC6: Greeting using substring method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -6,9 +6,17 @@ public class HelloApp {
             System.out.println("Hello, World!");
         } 
         else {
+
+            String names = "";
+
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                names += name + ", ";
             }
+
+            // Remove last comma and space using substring
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
 
     }
